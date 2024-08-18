141fcad66fc0dd92da9731a91b930e36c7c49e8b
edidiesky <essienedidiong1000@gmail.com>
2024-08-18 15:11:58 -0700

    new fixes

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,4 +1,15 @@
 import { Link } from "react-router-dom";
+import { FaLongArrowAltRight } from "react-icons/fa";
+import { Github, Linkedin, Twitter } from "lucide-react";
+
+const list = [
+  "full body piercing",
+  "traditional tattoos",
+  "tiny or micro tattoos",
+  "realism tattoos",
+  "geometric tattoos",
+  "laser tattoo removal",
+];
 
 const navbarCenterList = [
   {
@@ -24,9 +35,53 @@ const navbarCenterList = [
 ];
 const Footer = () => {
   return (
-    <>
-    
-    </>
+    <div className="flex flex-col w-full">
+      <div className="w-full background py-40 pb-20">
+        <div className="w-[90%] max-w-custom mx-auto grid md:grid-cols-custom gap-8">
+          <div className="w-full grid md:grid-cols-2 gap-6">
+            {list?.map((data, index) => {
+              return (
+                <span
+                  key={index}
+                  className="text-3xl md:text-4xl cursor-pointer capitalize family2 text-[var(--pink)] hover:text-[var(--primary)]"
+                >
+                  {data}
+                </span>
+              );
+            })}
+          </div>
+          <div className="w-[500px] flex flex-col gap-4">
+            <h4 className="text-center family2 text-3xl capitalize font-normal  text-[#fff]">
+              subscribe to our newsletter
+            </h4>
+            <div
+              className="flex-1 border-b flex items-center gap-4 family3 bg-transparent
+             px-4 border-[rgba(255,255,255,.5)] text-lg outline-none"
+            >
+              <input
+                className="bg-transparent flex-1 px-4"
+                type="text"
+                placeholder="Full Name"
+              />
+              <div className="btn px-8 py-4 text-white capitalize text-base">
+                Sign Up
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="w-full background_grey py-8">
+        <h4 className="text-center family3 text-sm font-normal  text-[#fff]">
+          <span className="text-[var(--primary)]">© 2024</span> • All Rights
+          Reserved •{" "}
+          <span className="text-[var(--primary)]">Tattoverse is a </span> tattoo
+          conceptualization •{" "}
+          <span className="text-[var(--primary)]">
+            Developed By Victor Essien
+          </span>
+        </h4>
+      </div>
+    </div>
   );
 };
 
--- src/constants/utils/SmoothScroll.jsx
+++ src/constants/utils/SmoothScroll.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useRef } from "react";
 // import { ReactLenis, useLenis } from "@studio-freight/react-lenis";
 const SmoothScroll = ({ children }) => {
 
-  return <div root>{children}</div>;
+  return <div>{children}</div>;
 };
 
 export default SmoothScroll;
